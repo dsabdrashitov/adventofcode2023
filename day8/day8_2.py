cda846a322554b15e616d2c319c1e62ab088aab7
dsabdrashitov <dsabdrashitov@mail.ru>
2023-12-21 18:09:33 +0300

    Day 8 part 2 another slow.

--- day8/day8_2.py
+++ day8/day8_2.py
@@ -1,4 +1,7 @@
 import re
+import logging
+
+_logger = logging.getLogger(__name__)
 
 # NAME = "sample.in"
 NAME = "input.txt"
@@ -7,18 +10,55 @@ START_PAT = re.compile(r'[0-9A-Z]*A')
 FINISH_PAT = re.compile(r'[0-9A-Z]*Z')
 
 
-def find_path(g, p, start: set, finish: set):
-    curs = start
-    curi = 0
-    done = 0
-    while not curs.issubset(finish):
-        nexts = set()
-        for s in curs:
-            nexts.add(g[s][p[curi]])
-        curs = nexts
-        curi = (curi + 1) % len(p)
+def go_precycle(node, done, g, p, finish):
+    while node not in finish:
+        node = g[node][p[done % len(p)]]
         done = done + 1
-    return done
+    return node, done
+
+
+def remove_precycle(g, p, start, finish):
+    started = set()
+    for node in start:
+        started.add(go_precycle(node, 0, g, p, finish))
+    return started
+
+
+def find_next(node, done, g, p, finish, graph_cache):
+    if (node, done % len(p)) in graph_cache:
+        return graph_cache[(node, done % len(p))]
+    node0 = node
+    node = g[node][p[done % len(p)]]
+    dist = 1
+    while node not in finish:
+        node = g[node][p[(done + dist) % len(p)]]
+        dist = dist + 1
+    graph_cache[(node0, done % len(p))] = node, dist
+    return node, dist
+
+
+def find_path(g, p, start: set, finish: set):
+    graph_cache = {}
+    current: set = remove_precycle(g, p, start, finish)
+    while True:
+        min_done = None
+        max_done = None
+        for node, done in current:
+            if min_done is None or min_done >= done:
+                min_done = done
+            if max_done is None or max_done <= done:
+                max_done = done
+        if min_done == max_done:
+            return min_done
+        logging.debug(f"{min_done}, {max_done}")
+        to_update = []
+        for node, done in current:
+            if done == min_done:
+                to_update.append((node, done))
+        for node, done in to_update:
+            current.remove((node, done))
+            nxt, dist = find_next(node, done, g, p, finish, graph_cache)
+            current.add((nxt, done + dist))
 
 
 def solve(f):
@@ -57,4 +97,5 @@ def main():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     main()
