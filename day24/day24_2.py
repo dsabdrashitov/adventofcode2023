e9af820663066fe15b7eec46a9179922619ff1f9
dsabdrashitov <dsabdrashitov@mail.ru>
2023-12-25 11:56:32 +0300

    Day 24 - experiments with equations.

--- day24/day24_2.py
+++ day24/day24_2.py
@@ -9,10 +9,10 @@ NAME = "sample.in"
 
 
 def hit_all(lines):
-    n = len(lines)
-    i0 = 2
-    i1 = 39
-    i2 = 63
+    i0 = 0
+    i1 = 1
+    i2 = 2
+    i3 = 3
     x1 = lines[i0][0][0]
     y1 = lines[i0][0][1]
     z1 = lines[i0][0][2]
@@ -22,34 +22,38 @@ def hit_all(lines):
     x3 = lines[i2][0][0]
     y3 = lines[i2][0][1]
     z3 = lines[i2][0][2]
-    vx1 = lines[i0][1][0]
-    vy1 = lines[i0][1][1]
-    vz1 = lines[i0][1][2]
-    vx2 = lines[i1][1][0]
-    vy2 = lines[i1][1][1]
-    vz2 = lines[i1][1][2]
-    vx3 = lines[i2][1][0]
-    vy3 = lines[i2][1][1]
-    vz3 = lines[i2][1][2]
+    x4 = lines[i3][0][0]
+    y4 = lines[i3][0][1]
+    z4 = lines[i3][0][2]
+    u1 = lines[i0][1][0]
+    v1 = lines[i0][1][1]
+    w1 = lines[i0][1][2]
+    u2 = lines[i1][1][0]
+    v2 = lines[i1][1][1]
+    w2 = lines[i1][1][2]
+    u3 = lines[i2][1][0]
+    v3 = lines[i2][1][1]
+    w3 = lines[i2][1][2]
+    u4 = lines[i3][1][0]
+    v4 = lines[i3][1][1]
+    w4 = lines[i3][1][2]
 
     def f(p):
-        x0, y0, z0, vx0, vy0, vz0, t1, t2, t3 = p
+        x0, y0, z0, u0, v0, w0 = p
         return (
-                x1 + vx1 * t1 - x0 - vx0 * t1,
-                y1 + vy1 * t1 - y0 - vy0 * t1,
-                z1 + vz1 * t1 - z0 - vz0 * t1,
-                x2 + vx2 * t2 - x0 - vx0 * t2,
-                y2 + vy2 * t2 - y0 - vy0 * t2,
-                z2 + vz2 * t2 - z0 - vz0 * t2,
-                x3 + vx3 * t3 - x0 - vx0 * t3,
-                y3 + vy3 * t3 - y0 - vy0 * t3,
-                z3 + vz3 * t3 - z0 - vz0 * t3
+            y1 * vx1 - y1 * vx0 + vy1 * x0 - vy1 * x1 - y0 * vx1 + y0 * vx0 - vy0 * x0 + vy0 * x1,
+            y2 * vx2 - y2 * vx0 + vy2 * x0 - vy2 * x2 - y0 * vx2 + y0 * vx0 - vy0 * x0 + vy0 * x2,
+            y3 * vx3 - y3 * vx0 + vy3 * x0 - vy3 * x3 - y0 * vx3 + y0 * vx0 - vy0 * x0 + vy0 * x3,
+            z1 * vx1 - z1 * vx0 + vz1 * x0 - vz1 * x1 - z0 * vx1 + z0 * vx0 - vz0 * x0 + vz0 * x1,
+            z2 * vx2 - z2 * vx0 + vz2 * x0 - vz2 * x2 - z0 * vx2 + z0 * vx0 - vz0 * x0 + vz0 * x2,
+            z3 * vx3 - z3 * vx0 + vz3 * x0 - vz3 * x3 - z0 * vx3 + z0 * vx0 - vz0 * x0 + vz0 * x3,
         )
 
-    x0, y0, z0, vx0, vy0, vz0, t1, t2, t3 = 0, 0, 0, 0, 0, 0, 0, 0, 0
+    x0, y0, z0, vx0, vy0, vz0 = 0, 0, 0, 0, 0, 0
     for i in range(100):
-        x0, y0, z0, vx0, vy0, vz0, t1, t2, t3 = fsolve(f, (x0, y0, z0, vx0, vy0, vz0, t1, t2, t3))
-    print(x0, y0, z0)
+        x0, y0, z0, vx0, vy0, vz0 = fsolve(f, (x0, y0, z0, vx0, vy0, vz0))
+    print(x0, y0, z0, "@", vx0, vy0, vz0)
+    print(f((x0, y0, z0, vx0, vy0, vz0)))
     answer = (x0, y0, z0), (vx0, vy0, vz0)
     return answer
 
