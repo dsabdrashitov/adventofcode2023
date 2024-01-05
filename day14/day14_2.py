07d7070e7edac37768b39e14df048d73280e4a0e
dsabdrashitov <dsabdrashitov@mail.ru>
2024-01-05 03:35:45 +0300

    Day 14 part 2.

--- day14/day14_2.py
+++ day14/day14_2.py
@@ -2,8 +2,8 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-NAME = "sample.in"
-# NAME = "input.txt"
+# NAME = "sample.in"
+NAME = "input.txt"
 
 CYCLES = 1000000000
 
@@ -12,16 +12,7 @@ ROUND = 'O'
 FREE = '.'
 
 
-def collect_pos(field, count):
-    result = [(-1, -1)] * count
-    for i in range(len(field)):
-        for j in range(len(field[i])):
-            if isinstance(field[i][j], int):
-                result[field[i][j]] = (i, j)
-    return result
-
-
-def do_cycle(field, do_debug = False):
+def do_cycle(field, do_debug=False):
     ground = [0] * len(field[0])
     for i in range(len(field)):
         for j in range(len(field[i])):
@@ -29,7 +20,7 @@ def do_cycle(field, do_debug = False):
                 pass
             elif field[i][j] == FIXED:
                 ground[j] = i + 1
-            elif isinstance(field[i][j], int):
+            elif field[i][j] == ROUND:
                 tmp = field[i][j]
                 field[i][j] = FREE
                 field[ground[j]][j] = tmp
@@ -39,7 +30,7 @@ def do_cycle(field, do_debug = False):
     if do_debug:
         _logger.debug("NORTH:")
         for x in field:
-            _logger.debug(''.join([ROUND if isinstance(obj, int) else obj for obj in x]))
+            _logger.debug(''.join(x))
     ground = [0] * len(field)
     for i in range(len(field)):
         for j in range(len(field[i])):
@@ -47,7 +38,7 @@ def do_cycle(field, do_debug = False):
                 pass
             elif field[i][j] == FIXED:
                 ground[i] = j + 1
-            elif isinstance(field[i][j], int):
+            elif field[i][j] == ROUND:
                 tmp = field[i][j]
                 field[i][j] = FREE
                 field[i][ground[i]] = tmp
@@ -57,7 +48,7 @@ def do_cycle(field, do_debug = False):
     if do_debug:
         _logger.debug("WEST:")
         for x in field:
-            _logger.debug(''.join([ROUND if isinstance(obj, int) else obj for obj in x]))
+            _logger.debug(''.join(x))
     ground = [len(field)] * len(field[0])
     for i in range(len(field) - 1, -1, -1):
         for j in range(len(field[i]) - 1, -1, -1):
@@ -65,7 +56,7 @@ def do_cycle(field, do_debug = False):
                 pass
             elif field[i][j] == FIXED:
                 ground[j] = i
-            elif isinstance(field[i][j], int):
+            elif field[i][j] == ROUND:
                 tmp = field[i][j]
                 field[i][j] = FREE
                 field[ground[j] - 1][j] = tmp
@@ -75,7 +66,7 @@ def do_cycle(field, do_debug = False):
     if do_debug:
         _logger.debug("SOUTH:")
         for x in field:
-            _logger.debug(''.join([ROUND if isinstance(obj, int) else obj for obj in x]))
+            _logger.debug(''.join(x))
     ground = [len(field[0])] * len(field)
     for i in range(len(field) - 1, -1, -1):
         for j in range(len(field[i]) - 1, -1, -1):
@@ -83,7 +74,7 @@ def do_cycle(field, do_debug = False):
                 pass
             elif field[i][j] == FIXED:
                 ground[i] = j
-            elif isinstance(field[i][j], int):
+            elif field[i][j] == ROUND:
                 tmp = field[i][j]
                 field[i][j] = FREE
                 field[i][ground[i] - 1] = tmp
@@ -93,7 +84,11 @@ def do_cycle(field, do_debug = False):
     if do_debug:
         _logger.debug("EAST:")
         for x in field:
-            _logger.debug(''.join([ROUND if isinstance(obj, int) else obj for obj in x]))
+            _logger.debug(''.join(x))
+
+
+def collect(field):
+    return ''.join([''.join(field[i]) for i in range(len(field))])
 
 
 def solve(f):
@@ -101,42 +96,26 @@ def solve(f):
     for line in f:
         line = line.strip()
         field.append([line[i] for i in range(len(line))])
-    count = 0
-    for i in range(len(field)):
-        for j in range(len(field[i])):
-            if field[i][j] == ROUND:
-                field[i][j] = count
-                count = count + 1
-    pos = [collect_pos(field, count)]
-    period = [0] * count
-    periods = 0
-    wait_done = 0
+    history = {collect(field): 0}
     done = 0
     while done < CYCLES:
         do_cycle(field, done == 0)
         done = done + 1
-        pos.append(collect_pos(field, count))
-        for j in range(count):
-            if period[j] > 0:
-                if pos[-1][j] != pos[-1 - period[j]][j]:
-                    period[j] = 0
-                    periods = periods - 1
-                else:
-                    continue
-            for k in range(1, done):
-                if pos[-1][j] == pos[-1 - k][j]:
-                    period[j] = k
-                    wait_done = max(wait_done, done + period[j])
-                    periods = periods + 1
-                    break
-        if periods == count and done > wait_done:
+        cf = collect(field)
+        if cf in history:
             break
-    _logger.info(period)
+        else:
+            history[cf] = done
+    _logger.info(f"{history[collect(field)]} -> {done}")
+    skip = done - history[collect(field)]
+    rem = (CYCLES - done) % skip
+    for _ in range(rem):
+        do_cycle(field)
     answer = 0
-    for i in range(count):
-        rem = (CYCLES - done) % period[i]
-        p = pos[-1 - period[i] + rem][i]
-        answer = answer + len(field) - p[0]
+    for i in range(len(field)):
+        for j in range(len(field[i])):
+            if field[i][j] == ROUND:
+                answer = answer + len(field) - i
     return answer
 
 
